8a6ae1dbfed18053e549bebf6f0380bb8d4b6c1d
Matias <matiasroma_128@hotmail.com>
2026-08-30 13:33:41 -0300

    usar DATABASE_URL

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,16 +2,12 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
 });
 
 pool.on('error', (err) => {
-  console.error('Error inesperado en el pool de PostgreSQL', err);
-  process.exit(-1);
+  console.error('Error en el pool de PostgreSQL', err);
 });
 
 module.exports = pool;
